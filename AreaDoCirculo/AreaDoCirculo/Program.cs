495da58b5ad52043c8bf340a8b888949c39e7e52
diegogalli <84423207+diegogalli@users.noreply.github.com>
2021-08-11 13:46:14 -0300

    Uri 1002 area do circulo

---
+++ AreaDoCirculo/AreaDoCirculo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace AreaDoCirculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double R, A, n = 3.14159;
+
+            R = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            A = n * R * R;
+
+            Console.WriteLine("A=" + A.ToString("F4", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
